bb8ebebc01268ecae389fb9ee9fc51ad7569a953
2liter <jacky23212303@gmail.com>
2017-10-17 22:38:01 +0800

    posd

---
+++ atom.cpp
@@ -0,0 +1,20 @@
+#include "atom.h"
+#include "variable.h"
+using std::string;
+
+
+Atom::Atom (string s):_symbol(s) {}
+
+string Atom::symbol() const{
+  return _symbol;
+}
+
+bool Atom::match(Term & term){
+    Variable *var_ps = dynamic_cast<Variable *>(&term);
+    if (var_ps)
+    {
+        Atom atom (_symbol);
+        var_ps->match(atom);
+        return true;
+    }
+}
